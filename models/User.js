45c96e3f0cb95a4fc15104c7bfcf0a8356626ae7
Amara Mehdi <amaramehdi986@gmail.com>
2025-01-12 04:21:11 +0100

    Add detailed error logging and improve user model

--- models/User.js
+++ models/User.js
@@ -21,11 +21,12 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ['student', 'admin'],
         default: 'student'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
-}, {
-    timestamps: true
 });
 
 module.exports = mongoose.model('User', userSchema); 
